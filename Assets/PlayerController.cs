37cac23e4c1e682e5d817643ea8a70479c2ed9bc
prestonwong7 <prestonwong7@gmail.com>
2018-08-14 23:47:44 -0700

    Movements and looking added, no jump

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+	void Start () {
+        motor = GetComponent<PlayerMotor>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        Vector3 velocty = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(velocty);
+
+        // Y rotation
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        //Apply rotation now
+        motor.Rotate(rotation);
+
+        //X rotation
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRotation, 0f, 0f) * lookSensitivity;
+
+        //Apply rotation of camera now
+        motor.RotateCamera(cameraRotation);
+    }
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))] 
+public class PlayerMotor : MonoBehaviour {
+
+    public Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    // Run every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation)); // I don't know what Quaternion does
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+    
+   
+}
